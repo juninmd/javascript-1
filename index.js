dfdc8734d606a9e811938b84601186da282c625e
Antonio Carlos <jr_acn@yahoo.com.br>
2021-10-23 13:08:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// Realizando ifs alternários
 const age = 12;
 const ageGroup = age > 18 ? "Adulto" : "Criança";
 console.log(ageGroup);
+
+
+// Exemplo: Aqui podemos criar objetos (entidades)
+const pessoa = {
+  nome: 'Rafa',
+  idade: 24,
+  cargo: 'Desenvolvedoraaaaa'
+};
+console.log(pessoa.nome); // Rafa
+
+// Destruct de objetos
+const { nome, idade, cargo } = pessoa;
+console.log(nome); // Rafa
+console.log(idade); // 24
+console.log(cargo); // Desenvolvedoraaaaa
